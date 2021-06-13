8d17466ffbc1a915eee9eb6439746e6338722319
tranthaison1231 <thaison.coderstokyo@gmail.com>
2021-06-13 11:46:08 +0700

    Setup project

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,2 +1,5 @@
 echo "sdk.dir = $ANDROID_HOME" > ./android/local.properties 
 cp .env.example .env.development
+cp .env.example .env.staging
+cp .env.example .env.production
+
--- src/Store/Pokemon.ts
+++ src/Store/Pokemon.ts
@@ -1,6 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import Config from 'react-native-config'
-global.fetch = require('node-fetch')
 
 export const pokemonApi = createApi({
   reducerPath: 'pokemonApi',
